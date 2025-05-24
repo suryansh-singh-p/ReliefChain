668714fbdd01f9cb13b858e61e63991762a71c04
Suryansh <singhsuryansh28parihar@gmail.com>
2025-05-24 07:29:15 +0530

    Axios debuged

--- client/src/Axiosapi.jsx
+++ client/src/Axiosapi.jsx
@@ -2,8 +2,12 @@ import axios from "axios";
 import dotenv from 'dotenv';
 
 dotenv.config();
-const baseURL = process.env.REACT_APP_API_URL; // Adjust the port as needed
+const baseURL = import.meta.env.VITE_API_URL; // Adjust the port as needed
 
 export default axios.create({
     baseURL: baseURL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
+
