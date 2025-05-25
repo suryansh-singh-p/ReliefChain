e0bc46a8c9e09caa2f11c75e1ce8d2937c9c8454
Suryansh <singhsuryansh28parihar@gmail.com>
2025-05-25 08:09:37 +0530

    Fix case-sensitive import for User model

--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User.js');
+const User = require('../models/User');
 const auth = require('../middleware/auth');
 const dotenv = require('dotenv');
 dotenv.config();
