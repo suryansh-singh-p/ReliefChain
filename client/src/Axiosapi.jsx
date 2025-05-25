1631c9f7981f10b3f21f2fbfb16a4b5745e8f96e
Suryansh <singhsuryansh28parihar@gmail.com>
2025-05-25 07:43:35 +0530

    Deleted app.js in backend

--- client/src/Axiosapi.jsx
+++ client/src/Axiosapi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const baseURL = import.meta.env.VITE_API_URL;
 
 const instance = axios.create({
     baseURL: baseURL,
--- server/server.js
+++ server/server.js
@@ -72,3 +72,5 @@ const PORT = 3000;
 httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
